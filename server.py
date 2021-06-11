b59426de8e7597ba920e27d418385d471254f2cb
angrit <andreas.greiss@web.de>
2021-06-11 14:37:49 +0200

    check replication msg

--- server.py
+++ server.py
@@ -490,8 +490,9 @@ class Server:
     def run_secondary(self):
         # self.replication_obj.create_multicast_sender()
         while True:
-            print("return of rep msg: {}".format(self.replication_obj.get_replication_message()))
+            # print("return of rep msg: {}".format(self.replication_obj.get_replication_message()))
             temp_dict_str = self.replication_obj.get_replication_message()
+            print(temp_dict_str)
             temp_dict = json.loads(temp_dict_str)
             if len(temp_dict) != 0:
                 for k, v in temp_dict.items():
